35d0b680d428796c1282cc586dc65d0ff83dcfd0
kheller18 <keenanaheller@gmail.com>
2023-02-09 15:09:55 -0800

    updated lambda function to check and handle inputs against constraints.

--- Lambda/lambda_function.py
+++ Lambda/lambda_function.py
@@ -1,6 +1,8 @@
 ### Required Libraries ###
+import json
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+from botocore.vendored import requests
 
 ### Functionality Helper Functions ###
 def parse_int(n):
@@ -80,38 +82,6 @@ def close(session_attributes, fulfillment_state, message):
     return response
 
 
-"""
-Step 3: Enhance the Robo Advisor with an Amazon Lambda Function
-
-In this section, you will create an Amazon Lambda function that will validate the data provided by the user on the Robo Advisor.
-
-1. Start by creating a new Lambda function from scratch and name it `recommendPortfolio`. Select Python 3.7 as runtime.
-
-2. In the Lambda function code editor, continue by deleting the AWS generated default lines of code, then paste in the starter code provided in `lambda_function.py`.
-
-3. Complete the `recommend_portfolio()` function by adding these validation rules:
-
-    * The `age` should be greater than zero and less than 65.
-    * The `investment_amount` should be equal to or greater than 5000.
-
-4. Once the intent is fulfilled, the bot should respond with an investment recommendation based on the selected risk level as follows:
-
-    * **none:** "100% bonds (AGG), 0% equities (SPY)"
-    * **low:** "60% bonds (AGG), 40% equities (SPY)"
-    * **medium:** "40% bonds (AGG), 60% equities (SPY)"
-    * **high:** "20% bonds (AGG), 80% equities (SPY)"
-
-> **Hint:** Be creative while coding your solution, you can have all the code on the `recommend_portfolio()` function, or you can split the functionality across different functions, put your Python coding skills in action!
-
-5. Once you finish coding your Lambda function, test it using the sample test events provided for this Challenge.
-
-6. After successfully testing your code, open the Amazon Lex Console and navigate to the `recommendPortfolio` bot configuration, integrate your new Lambda function by selecting it in the “Lambda initialization and validation” and “Fulfillment” sections.
-
-7. Build your bot, and test it with valid and invalid data for the slots.
-
-"""
-
-
 ### Intents Handlers ###
 def recommend_portfolio(intent_request):
     """
@@ -124,8 +94,88 @@ def recommend_portfolio(intent_request):
     risk_level = get_slots(intent_request)["riskLevel"]
     source = intent_request["invocationSource"]
 
-    # YOUR CODE GOES HERE!
+    # validates slots based on DialogCodeHook event
+    if source == 'DialogCodeHook':
+        all_slots = get_slots(intent_request)
 
+        # validate inputs from lex age and investment amount 
+        validation_result = validate_inputs(age, investment_amount)
+
+        if not validation_result['isValid']:
+            all_slots[validation_result["violatedSlot"]] = None
+
+            # returns asking for new data if inputs don't match constraints
+            return elicit_slot(
+                intent_request["sessionAttributes"],
+                intent_request["currentIntent"]["name"],
+                all_slots,
+                validation_result["violatedSlot"],
+                validation_result["message"]
+            )
+
+        # Fetch current session attributes
+        session_attributes = intent_request["sessionAttributes"]
+
+        # Once all slots are valid, a delegate dialog is returned to Lex to choose the next course of action.
+        return delegate(session_attributes, all_slots)
+
+    # required slots are validated and ready to provide portfolio recommendation
+    portfolio_recommendation = get_recommendation(risk_level)
+
+    # final return message with recommended portfolio percentages
+    return close(
+        intent_request["sessionAttributes"],
+        "Fulfilled",
+        {
+            "contentType": "PlainText",
+            "content": f"Thank you! The recommended diversification for you based on your risk level is: {portfolio_recommendation}"
+        },
+    )
+
+
+### gives advice based users' choice of aggressiveness ###
+def get_recommendation(risk_level):
+    """Returns percentage split for bonds and equities based on users' agressiveness"""
+
+    if risk_level == 'None':
+        return '100% bonds (AGG), 0% equities (SPY).'
+    elif risk_level == 'Low':
+        return '60% bonds (AGG), 40% equities (SPY).'
+    elif risk_level == 'Medium':
+        return '40% bonds (AGG), 60% equities (SPY).'
+    elif risk_level == 'High':
+        return '20% bonds (AGG), 80% equities (SPY).'
+    else:
+        return '100% bonds (AGG), 0% equities (SPY) - (Invalid risk-level, default portfolio recommended).'
+
+
+### validates age and investment amount ###
+def validate_inputs(age, investment_amount):
+    """ Checks to make sure age is valid so you don't try and change NaN to float """
+    if age is not None:
+        age = float(age)
+
+        """ Age constraint """
+        if age <= 0 or age >= 65:
+            return build_validation_result(
+                False,
+                'age',
+                'The age you entered is invalid. Please enter an age between 0 and 65.'
+            )
+
+    """ Checks to make sure investment amount is valid so you don't try and change NaN to float """
+    if investment_amount is not None:
+        investment_amount = float(investment_amount)
+
+        """ Checks against constraint """
+        if investment_amount < 5000:
+            return build_validation_result(
+                False,
+                'investmentAmount',
+                'The investment amount you entered is invalid. Please enter an amount greater than or equal to $5000.'
+            )
+
+    return build_validation_result(True, None, None)
 
 ### Intents Dispatcher ###
 def dispatch(intent_request):
